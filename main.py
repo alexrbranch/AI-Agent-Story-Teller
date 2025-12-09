f8aa65f3e21ded25ff603fbaa7f7aff8d399eb57
Alex Branch <alexbranchpb@gmail.com>
2025-12-09 15:48:25 +0700

    Use an outliner agent. This multi-agent structure offloads the entire task from the writer agent. Added logging to record agent interaction.

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import os
+import json
+import logging
+import time
 from openai import OpenAI
 from dotenv import load_dotenv
+
+
 import configs
 
 """
@@ -8,12 +13,16 @@ Before submitting the assignment, describe here in a few sentences what you woul
 
 """
 
+# Simple logger: root config only
+logging.basicConfig(level=logging.INFO, filename=configs.LOG_FILE)
+logger = logging.getLogger("Storyteller")
+
 load_dotenv()
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 
 class Agent:
-    def __init__(self, role_name, system_prompt, model: configs.MODEL_NAME):
+    def __init__(self, role_name, system_prompt, model=configs.MODEL_NAME):
         self.role = role_name
         self.system_prompt = system_prompt
         self.model = model
@@ -39,14 +48,28 @@ def get_user_inputs():
 
     return input_theme, input_character_name
 
+def save_story(content):
+    with open(configs.STORY_OUTPUT_FILE, "w", encoding="utf-8") as f:
+        f.write(content)
+    print(f"Story saved to {configs.STORY_OUTPUT_FILE}")
 
 def main():
+    start_time = time.time()
+    logger.info(f"[{time.strftime('%Y-%m-%d %H:%M:%S')}] Starting storyteller...")
+
     name, theme = get_user_inputs()
+    user_request = f"Create a story about {theme} with the main character {name}."
+    logger.info("User request: \n{user_request}")
 
-    storyteller = Agent(role_name="Storyteller", system_prompt=configs.WRITER_SYSTEM_PROMPT, model=configs.MODEL_NAME)
+    outliner = Agent(role_name="Outliner", system_prompt=configs.OUTLINER_SYSTEM_PROMPT)
+    outline_raw = outliner.call_model(user_request) #json output
+    logger.info(f"Outline raw: \n{outline_raw}")
 
-    response = storyteller.call_model(f"Create a story about {theme} with the main character {name}.")
-    print(response)
+    storyteller = Agent(role_name="Storyteller", system_prompt=configs.WRITER_SYSTEM_PROMPT)
+    story_content = storyteller.call_model(outline_raw)
+    logger.info(f"Story content: \n{story_content}")
+    
+    save_story(story_content)
 
 
 if __name__ == "__main__":
